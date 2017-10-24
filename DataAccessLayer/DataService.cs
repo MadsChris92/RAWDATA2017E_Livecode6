f5384e9efaacd4d20752cb643532b213e2fccd7d
Henrik Bulskov <bulskov@ruc.dk>
2017-10-24 10:09:39 +0200

    Livecode 6

---
+++ DataAccessLayer/DataService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DataAccessLayer
+{
+    public class DataService
+    {
+        public IList<Category> GetCategories()
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Categories.ToList();
+            }
+        }
+
+        public Category GetCategory(int id)
+        {
+            using (var db = new NorthwindContext())
+            {
+                return db.Categories.Find(id);
+            }
+        }
+    }
+}
---
+++ DataAccessLayer/NorthwindContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer
+{
+    public class NorthwindContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseMySql("server=localhost;database=northwind;uid=bulskov;pwd=henrik");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Category>().Property(x => x.Id).HasColumnName("categoryid");
+            modelBuilder.Entity<Category>().Property(x => x.Name).HasColumnName("categoryname");
+        }
+    }
+}
