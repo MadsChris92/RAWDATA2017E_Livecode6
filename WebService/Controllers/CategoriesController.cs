288366ed299d29a35f5595b39949285e66c32074
Henrik Bulskov <bulskov@ruc.dk>
2017-10-24 10:46:03 +0200

    Add categories controller

---
+++ WebService/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebService.Controllers
+{
+    [Route("api/categories")]
+    public class CategoriesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            var ds = new DataService();
+            return Ok(ds.GetCategories());
+        }
+    }
+}
